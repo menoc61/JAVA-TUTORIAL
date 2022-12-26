eb0978f17b4bbf38219d2176bddff3e3dde9d67c
GILLE MOMENI <94852946+menoc61@users.noreply.github.com>
2022-12-26 21:32:38 +0100

    first java app

---
+++ stringDemo.java
@@ -0,0 +1,16 @@
+//File Name: stringDemo
+public class stringDemo {
+    public static void main(String[] args) {
+        float floatVar= 12;
+        int intVar =13;
+        String stringVar ="hello world";
+        
+        System.out.printf("The value of the float variable is "+"%f, while the value of the integer "+"variable is %d, and the string "+"is %s", floatVar, intVar, stringVar);
+        
+    }
+}
+//======================== NOTE ==================================================================
+// Pour les francophone desole encore une fois mais vous allez devoir utilise google translator🙇🏾‍♂️
+// https://translate.google.com/
+//=================================================================================================
+// 1.) On this tutorial you will learn the basic initiation of a java project 
