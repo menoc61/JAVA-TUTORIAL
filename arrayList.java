e244b971ac8258e4e74ca1806f702557a7bdf9c3
GILLE MOMENI <94852946+menoc61@users.noreply.github.com>
2022-12-26 22:50:30 +0100

    array list

---
+++ arrayList.java
@@ -0,0 +1,45 @@
+package importantJava;//import only if impprtantJava folder exist
+import java.util.*;
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<String> food = new ArrayList<String>();
+        food.add("Pizza");  //the .add() method is used to add elt to the array list
+        food.add("Humburger");
+        food.add("Hotdogs");
+        ArrayList<String> Drinks = new ArrayList<String>();
+        Drinks.add("soda");
+        Drinks.add("Jack daniel");
+        Drinks.add("Sano");
+        Drinks.add("Top orange");
+        ArrayList<String> Games = new ArrayList<String>();
+        Games.add("Fornite");
+        Games.add("God of war 3");
+        Games.add("Blur");
+        ArrayList<ArrayList<String>> ListOfThinks = new ArrayList<ArrayList<String>>();
+        ListOfThinks.add(food);
+        ListOfThinks.add(Drinks);
+        ListOfThinks.add(Games);
+
+        for (int i = 0; i < ListOfThinks.size(); i++) {
+            /*ListOfThinks can be used to get the content of all the array list that has been added to it
+            *ListOfThonks can be used to get the first list by .get(0);
+            *ListOfThonks can be used to get the element of the first list by .get(0).get(0);
+            */
+            System.out.println(ListOfThinks.get(0));
+        }
+
+        food.remove(2); //the remove method will remove the index 0 in our array list
+        //food.clear(); // delete completely the array list.
+        food.set(0,"Cake"); //the set() method will replace the value of with the index 0 in the list
+        for (int i = 0; i < food.size(); i++) {
+            System.out.println(food.get(i));
+
+
+        }
+    }
+}
+//======================== NOTE ==================================================================
+// Pour les francophone desole encore une fois mais vous allez devoir utilise google translator🙇🏾‍♂️
+// https://translate.google.com/
+//=================================================================================================
+// 1.) On this tutorial you will learn the use and inplementation of array list for more details look at w3school doc
